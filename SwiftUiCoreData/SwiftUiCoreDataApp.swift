6f3a1de6c22db9c669776bc415825e72430c85e8
saad <saadneosoft@gmail.com>
2023-11-09 11:53:11 +0530

    Initial Commit

---
+++ SwiftUiCoreData/SwiftUiCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiCoreDataApp.swift
+//  SwiftUiCoreData
+//
+//  Created by Neosoft on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
